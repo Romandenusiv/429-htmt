bfcdc0c2e1e6975b1815d4975263a6d5592e06b2
Roman Fedusiw <romanfedusiv@gmail.com>
2019-08-08 22:03:10 +0000

    Coll

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const  link = document.querySelector('a');
+link.textContent ='Pit Comyunity';
+
+const sect = document.querySelector('section');
+
+const para = document.createElement('p');
+para.textContent ='We hope that ride.';
+sect.appendChild(para);
